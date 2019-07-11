e7f303e18f7e534e7df5e0a40ff155b92d2fce5f
Aaron Chafetz <achafetz@usaid.gov>
2019-07-11 09:20:54 -0400

    add calcs to determine counts not just shares

--- R/ingest_regimens.R
+++ R/ingest_regimens.R
@@ -8,15 +8,27 @@ ingest_regimens <- function(filepath){
 
   month <- ingest_month(filepath)
 
-  df <- readxl::read_xlsx(filepath, skip = 2, sheet = "4. Current ART Regimens")
+  if(ingest_sheetexist(filepath, "4. Current ART Regimens")){
 
-  df <- df %>%
-    dplyr::filter(!is.na(regimen_type)) %>%
-    dplyr::mutate(month = month,
-                  category = "Regimen",
-                  regimen_mmd_enrolled =
-                    ifelse(is.na(regimen_mmd_enrolled) & regimen_mmd_enrolled_share > 0, "Yes", regimen_mmd_enrolled)) %>%
-    dplyr::select(month, dplyr::everything())
+    df <- readxl::read_xlsx(filepath, skip = 2, sheet = "4. Current ART Regimens", col_types = "text")
 
-  return(df)
+    df <- dplyr::mutate_at(df, dplyr::vars(dplyr::matches("count|share")), as.numeric)
+
+    df <- df %>%
+      dplyr::filter(!is.na(regimen_type)) %>%
+      dplyr::mutate(month = month,
+                    category = "Regimen",
+                    regimen_mmd_enrolled =
+                      ifelse(is.na(regimen_mmd_enrolled) & regimen_mmd_enrolled_share > 0, "Yes", regimen_mmd_enrolled),
+                    regimen_mmd_eligible_count = regimen_mmd_eligible_share * regimen_count,
+                    regimen_mmd_enrolled_count = regimen_mmd_enrolled_share * regimen_mmd_eligible_count) %>%
+      dplyr::select(month, dplyr::everything())
+
+    return(df)
+
+  } else {
+
+    return(NULL)
+
+  }
 }
